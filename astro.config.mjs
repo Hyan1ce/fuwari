6e2ab458dba019beabcaadac8502fa823c6f07e4
HyanIce <109321680+Hyan1ce@users.noreply.github.com>
2024-09-21 21:30:17 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -31,8 +31,8 @@ const oklchToHex = (str) => {
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://hyan1ce.github.io/src",
-  base: "/src/",
+  site: "https://hyan1ce.github.io",
+  base: "/",
   trailingSlash: "always",
   integrations: [
     tailwind(),
