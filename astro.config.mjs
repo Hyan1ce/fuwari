8aab6a8153a08421ea88db17db1fea0b43581341
Hyan1ce <hsaltice@gmail.com>
2024-09-21 20:38:49 +0800

    更新astro配置文件

--- astro.config.mjs
+++ astro.config.mjs
@@ -31,7 +31,7 @@ const oklchToHex = (str) => {
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://fuwari.vercel.app/",
+  site: "https://hyan1ce.github.io",
   base: "/",
   trailingSlash: "always",
   integrations: [
